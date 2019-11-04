57225c6cd481b4a652cb547b012c1e588f3ed30f
Ilya K <ikuz_2002@mail.ru>
2019-11-04 20:28:50 +0300

    Lab9 Task 2

--- Lab9/9.c
+++ Lab9/9.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    
+    //Task 2
+    unsigned monthlyFee, minutePriceIncreased, usedMinutes;
+    const unsigned freeMinutes = 499;
+
+    printf("=== Task 2 ===");
+    printf("Enter normal monthly fee: ");
+    scanf("%u", &monthlyFee);
+    printf("Enter increased price for each minute: ");
+    scanf("%u", &minutePriceIncreased);
+    printf("Enter used minutes");
+    scanf("%u", &usedMinutes);
+
+    unsigned result = monthlyFee;
+
+    if (usedMinutes - freeMinutes > 0) {
+        result += minutePriceIncreased * (usedMinutes - freeMinutes);
+    }
+
+    printf("Total cost is %u", result);
+
     return 0;
 }
