04e6fb0dcbb9e11cbef9ce9b44983f674eacf0c4
Ilya K <ikuz_2002@mail.ru>
2019-11-04 21:08:41 +0300

    Lab9 Task 2 & Task 5

--- Lab9/9.c
+++ Lab9/9.c
@@ -6,21 +6,40 @@ int main(int argc, char *argv[]) {
     unsigned monthlyFee, minutePriceIncreased, usedMinutes;
     const unsigned freeMinutes = 499;
 
-    printf("=== Task 2 ===");
+    printf("=== Task 2 ===\n");
     printf("Enter normal monthly fee: ");
     scanf("%u", &monthlyFee);
     printf("Enter increased price for each minute: ");
     scanf("%u", &minutePriceIncreased);
-    printf("Enter used minutes");
+    printf("Enter used minutes: ");
     scanf("%u", &usedMinutes);
 
     unsigned result = monthlyFee;
 
-    if (usedMinutes - freeMinutes > 0) {
+    if ((int)(usedMinutes - freeMinutes) > 0) {
         result += minutePriceIncreased * (usedMinutes - freeMinutes);
     }
 
-    printf("Total cost is %u", result);
+    printf("Total cost is %u\n", result);
+
+    //Task 5
+    printf("=== Task 5 ===\n");
+
+    double amount, rate;
+    unsigned period;
+
+    printf("Enter the initial amount of money: ");
+    scanf("%lf", &amount);
+    printf("Enter the interest rate (%%): ");
+    scanf("%lf", &rate);
+    printf("Enter the period of the deposit: ");
+    scanf("%u", &period);
+
+    rate /= 100;
+    for (int i = 1; i <= period; i++) {
+        amount *= 1 + rate / 12;
+        printf("The amount on the month %u is %f\n", i, amount);
+    }
 
     return 0;
 }
